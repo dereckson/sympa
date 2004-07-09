a551aa5e2bb09da341eee8bea5870522343a7947
nobody <nobody@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2004-07-09 14:37:33 +0000

    This commit was manufactured by cvs2svn to create tag 'sympa-4_2b_2'.

---
+++ tags/sympa-4_2b_2/src/Language.pm
@@ -0,0 +1,174 @@
+# Language.pm - This module does just the initial setup for the international messages
+# RCS Identication ; $Revision$ ; $Date$ 
+#
+# Sympa - SYsteme de Multi-Postage Automatique
+# Copyright (c) 1997, 1998, 1999, 2000, 2001 Comite Reseau des Universites
+# Copyright (c) 1997,1998, 1999 Institut Pasteur & Christophe Wolfhugel
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+package Language;
+
+require Exporter;
+use Carp;
+@ISA = qw(Exporter);
+@EXPORT = qw(Msg gettext);
+
+use strict;
+use Log;
+use Version;
+use POSIX qw (setlocale);
+use Locale::Messages qw (:locale_h :libintl_h);
+
+my %msghash;     # Hash organization is like Messages file: File>>Sections>>Messages
+my %set_comment; #sets-of-messages comment   
+
+## The lang is the NLS catalogue name ; locale is the locale preference
+## Ex: lang = fr ; locale = fr_FR
+my ($current_lang, $current_locale);
+my $default_lang;
+## This was the old style locale naming, used for templates, nls, scenario
+my %language_equiv = ( 'zh_CN' => 'cn',
+		       'zh_TW' => 'tw',
+		       'cs'    => 'cz',
+		       'en_US' => 'us',
+		       );
+
+## Supported languages
+my @supported_languages = ('cs_CZ','de_DE','en_US','es_ES','et_EE',
+			   'fi_FI','fr_FR','hu_HU','it_IT','nl_NL',
+			   'pl_PL','pt_PT','ro_RO','zh_CN','zh_TW');
+
+my %lang2locale = ('cz' => 'cs_CZ',
+		   'de' => 'de_DE',
+		   'us' => 'en_US',
+		   'es' => 'es_ES',
+		   'et' => 'et_EE',
+		   'fi' => 'fi_FI',
+		   'fr' => 'fr_FR',
+		   'hu' => 'hu_HU',
+		   'it' => 'it_IT',
+		   'nl' => 'nl_NL',
+		   'pl' => 'pl_PL',
+		   'pt' => 'pt_PT',
+		   'ro' => 'ro_RO',
+		   'cn' => 'zh_CN',
+		   'tw' => 'zh_TW');
+
+sub GetSupportedLanguages {
+    my $robot = shift;
+    my @lang_list;
+    
+    foreach my $l (split /,/,&Conf::get_robot_conf($robot, 'supported_lang')) {
+	push @lang_list, $lang2locale{$l}||$l;
+    }
+    return @lang_list;
+}
+
+sub SetLang {
+###########
+    my $locale = shift;
+    &do_log('debug', 'Language::SetLang(%s)', $locale);
+
+    my $lang = $locale;
+
+    unless ($lang) {
+	&do_log('err','Language::SetLang(), missing locale parameter');
+	return undef;
+    }
+
+   if (length($locale) == 2) {
+	$locale = $lang2locale{$lang};
+    }else {
+	## Get the NLS equivalent for the lang
+	if (defined $language_equiv{$lang}) {
+	    $lang = $language_equiv{$lang};
+	}else {
+	    ## remove the country part 
+	    $lang =~ s/_\w{2}$//;
+	}
+    }
+   
+    ## Set Locale::Messages context
+    unless (setlocale(&POSIX::LC_ALL, $locale)) {
+	&do_log('err','Failed to setlocale(%s)', $locale);
+	return undef;
+    }
+    &Locale::Messages::textdomain("sympa");
+    &Locale::Messages::bindtextdomain('sympa','--DIR--/locale');
+    #bind_textdomain_codeset sympa => 'iso-8859-1';
+
+    $current_lang = $lang;
+    $current_locale = $locale;
+
+    return $locale;
+}#SetLang
+
+sub GetLang {
+############
+
+    return $current_lang;
+}
+
+sub maketext {
+    my $msg = shift;
+
+#    &do_log('notice','Maketext: %s', $msg);
+
+    ## xgettext.pl bug adds a \n to multi-lined strings
+    if ($msg =~ /\n.+/m) {
+	$msg .= "\n";
+    }
+
+    my $translation = gettext ($msg);
+
+    ## replace parameters in string
+    $translation =~ s/\%(\d+)/$_[$1-1]/eg;
+
+    return $translation;
+}
+
+sub gettext {
+
+    ## This prevents meta information to be returned if the string to translate is empty
+    if ($_[0] eq '') {
+	return '';
+	
+	## return meta information on the catalogue (language, charset, encoding,...)
+    }elsif ($_[0] =~ '^_(\w+)_$') {
+	my $var = $1;
+	foreach (split /\n/,&Locale::Messages::gettext('')) {
+	    if ($var eq 'language') {
+		if (/^Language-Team:\s*(.+)$/i) {
+		    return $1;
+		}
+	    }elsif ($var eq 'charset') {
+		if (/^Content-Type:\s*.*charset=(\S+)$/i) {
+		    return $1;
+		}
+	    }elsif ($var eq 'encoding') {
+		if (/^Content-Transfer-Encoding:\s*(.+)$/i) {
+		    return $1;
+		}
+	    }
+	}
+	return '';
+    }
+
+    &Locale::Messages::gettext(@_);
+}
+
+1;
+
---
+++ tags/sympa-4_2b_2/src/mail.pm
@@ -0,0 +1,313 @@
+# mail.pm - This module includes mail sending functions
+# RCS Identication ; $Revision$ ; $Date$ 
+#
+# Sympa - SYsteme de Multi-Postage Automatique
+# Copyright (c) 1997, 1998, 1999, 2000, 2001 Comite Reseau des Universites
+# Copyright (c) 1997,1998, 1999 Institut Pasteur & Christophe Wolfhugel
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+package mail;
+
+require Exporter;
+use Carp;
+@ISA = qw(Exporter);
+@EXPORT = qw(mailback mailarc mailfile set_send_spool);
+
+#use strict;
+
+use Conf;
+use Log;
+use Language;
+use List;
+
+## RCS identification.
+#my $id = '@(#)$Id$';
+
+my $send_spool;
+
+sub set_send_spool {
+    my $spool = pop;
+
+    $send_spool = $spool;
+}
+
+## Mail back a response to the given address.
+## Data is a reference to an array or a scalar.
+sub mailback {
+   my($data, $headers, $from, $to, $robot, @rcpt) = @_;
+   do_log('debug2', 'mail::mailback(%s, %s)', $from, join(',', @rcpt));
+
+   my ($fh, $sympa_file);
+   
+   my $sympa_email =  &Conf::get_robot_conf($robot, 'sympa');
+
+   ## Don't fork if used by a CGI (FastCGI problem)
+   if (defined $send_spool) {
+       $sympa_file = "$send_spool/T.$sympa_email.".time.'.'.int(rand(10000));
+       my $rcpt = join ',', @rcpt;
+
+       unless (open TMP, ">$sympa_file") {
+	   &do_log('notice', 'Cannot create %s : %s', $sympa_file, $!);
+	   return undef;
+       }
+       
+       printf TMP "X-Sympa-To: %s\n", $rcpt;
+       printf TMP "X-Sympa-From: %s\n", $sympa_email;
+       printf TMP "X-Sympa-Checksum: %s\n", &tools::sympa_checksum($rcpt);
+       
+       $fh = \*TMP;
+   }else {
+       $fh = smtp::smtpto($sympa_email, \@rcpt);
+   }
+   
+   ## Charset for encoding
+   my $charset = sprintf (gettext("_charset_"));
+
+   printf $fh "To:  %s\n", MIME::Words::encode_mimewords($to, 'Q', $charset);
+   if ($from eq 'sympa') {
+       printf $fh "From: %s\n", MIME::Words::encode_mimewords((sprintf ("SYMPA <%s>", $sympa_email)), 'Q', $charset);
+   }else {
+       printf $fh "From: %s\n", MIME::Words::encode_mimewords($from, 'Q', $charset);
+   }
+   foreach my $field (keys %{$headers}) {
+       printf $fh "%s: %s\n", $field, MIME::Words::encode_mimewords($headers->{$field}, 'Q', $charset);
+   }
+   printf $fh "MIME-Version: 1.0\n";
+   printf $fh "Content-Type: text/plain; charset=%s\n", gettext("_charset_");
+   printf $fh "Content-Transfer-Encoding: %s\n", gettext("_encoding_");
+   print $fh "\n";
+
+   if (ref($data) eq 'SCALAR') {
+      print $fh $$data;
+   } elsif (ref($data) eq 'ARRAY') {
+      print $fh @$data;
+   }
+   close($fh);
+   
+   if (defined $sympa_file) {
+       my $new_file = $sympa_file;
+       $new_file =~ s/T\.//g;
+       
+       unless (rename $sympa_file, $new_file) {
+	   &do_log('notice', 'Cannot rename %s to %s : %s', $sympa_file, $new_file, $!);
+	   return undef;
+       }
+   }
+
+   return 1;
+}
+
+## send an archive file
+sub mailarc {
+   my($filename, $subject, @rcpt) = @_;
+   do_log('debug2', 'mail::mailarc(%s, %s)', $subject, join(',', @rcpt));
+
+   my($i);
+
+   if (!open(IN, $filename)) {
+      fatal_err("Can't send %s to %s: %m", $filename, join(',', @rcpt));
+   }
+   my($fh) = &smtp::smtpto($Conf{'robots'}{$robot}{'sympa'} || $Conf{'request'}, \@rcpt);
+   printf $fh "To: %s\n", join(",\n   ", @rcpt);
+   print $fh "Subject: $subject\n";
+   printf $fh "MIME-Version: 1.0\n";
+   printf $fh "Content-Type: text/plain; charset=%s\n", gettext("_charset_");
+   printf $fh "Content-Transfer-Encoding: %s\n", gettext("_encoding_");
+   print $fh "\n";
+   print $fh $i while ($i = <IN>);
+   close($fh);
+}
+
+## send welcome, bye, expire removed or reminder message to a user
+sub mailfile {
+   my ($filename, $rcpt, $data, $robot, $sign_mode) = @_;
+   do_log('debug2', 'mail::mailfile(%s, %s, %s, %s)', $filename, $rcpt, $robot, $sign_mode);
+
+   my ($full_msg, $return_path, $sendmail, $to, $sympa_file);
+
+   ## We may receive a list a recepients
+   
+   if (ref ($rcpt)) {
+       unless (ref ($rcpt) eq 'ARRAY') {
+	   &do_log('notice', 'Wrong type of reference for rcpt');
+	   return undef;
+       }
+
+#       if ($sign_mode eq 'smime') {
+#	   &do_log('notice', 'Cannot sign a message with multiple recepients');
+#	   return undef;
+#       }
+
+       if ($data->{'to'}) {
+	   $to = $data->{'to'};
+       }else {
+	   $to = join(",\n   ", @{$rcpt});
+       }
+   }else{
+       $to = $rcpt;
+   }   
+
+   ## Get a FD
+#   unless ($sign_mode eq 'smime') {
+
+       ## Don't fork if used by a CGI (FastCGI problem)
+       if (defined $send_spool) {
+	   my $sympa_email = $data->{'conf'}{'sympa'} || &Conf::get_robot_conf($robot, 'sympa');
+	   $sympa_file = "$send_spool/T.$sympa_email.".time.'.'.int(rand(10000));
+	   
+	   unless (open TMPMSG, ">$sympa_file") {
+	       &do_log('notice', 'Cannot create %s : %s', $sympa_file, $!);
+	       return undef;
+	   }
+
+	   my $all_rcpt = $rcpt;
+	   if (ref($rcpt)) {
+	       $all_rcpt = join (',', @$rcpt);
+	   }
+
+	   printf TMPMSG "X-Sympa-To: %s\n", $all_rcpt;
+	   printf TMPMSG "X-Sympa-From: %s\n", $data->{'return_path'};
+	   printf TMPMSG "X-Sympa-Checksum: %s\n", &tools::sympa_checksum($all_rcpt);
+	   
+	   $sendmail = \*TMPMSG;
+       }else {
+	   
+	   if (ref ($rcpt)) {
+	       $sendmail = &smtp::smtpto($data->{'return_path'}, $rcpt);
+	   }else {
+	       $sendmail = &smtp::smtpto($data->{'return_path'}, \$rcpt);
+	   }
+       }
+#  }
+
+   ## Does the file include headers ?
+   if ($filename =~ /\.tt2$/) {
+       open TPL, $filename;
+       my $first_line = <TPL>;
+       $full_msg = 1 if ($first_line =~ /^From:\s/);
+       close TPL;
+   }
+
+   ## If message needs to be signed
+   my ($fh, $tmp_file);
+   if ($sign_mode eq 'smime') {
+       $tmp_file = $Conf{'tmpdir'}.'/sympa_mailfile_'.time.'.'.$$;
+
+       unless (open TMPSMIME, ">$tmp_file") {
+	   &do_log('notice', 'Cannot create %s : %s', $tmp_file, $!);
+	   return undef;
+       }
+
+       $fh = \*TMPSMIME;
+   }else {
+       $fh = $sendmail;
+   }
+
+   printf $fh "To: %s\n", $to;
+
+
+   ## Not a complete MIME message
+   unless ( $full_msg or ($filename =~ /\.mime$/) ){
+       print $fh "From: $data->{'from'}\n";
+       print $fh "Subject: $data->{'subject'}\n";
+       print $fh "Reply-to: $data->{'replyto'}\n" if ($data->{'replyto'}) ;
+       printf $fh "MIME-Version: 1.0\n";
+       printf $fh "Content-Type: text/plain; charset=%s\n", gettext("_charset_");
+       printf $fh "Content-Transfer-Encoding: %s\n", gettext("_encoding_");
+       print $fh "\n";
+   }
+
+   if ($filename) {
+       if ($filename =~ /\.tt2$/) {
+	   #&parser::parse_tpl($data, $filename, \$fh);
+	   my $output;
+	   &tt2::parse_tt2($data, $filename, \$output);
+	   #warn 'from tmpl:',join('',$output);
+	   print $fh join('',$output);
+
+       }else {
+	   ## Old style
+	   open IN, $filename;
+	   while (<IN>) {
+	       s/\[listname\]/$data->{'list'}{'name'}/g;
+	       s/\[subscriber_email\]/$data->{'user'}{'email'}/g;
+	       s/\[email_subscriber\]/$data->{'user'}{'email'}/g;
+	       s/\[subscriber_gecos\]/$data->{'user'}{'gecos'}/g;
+	       s/\[sympa_email\]/$data->{'conf'}{'sympa'}/g;
+	       s/\[sympa_host\]/$data->{'conf'}{'host'}/g;
+	       print $fh $_ ;
+	   }
+	   close IN;
+       }
+   }else{
+       print $fh $data->{'body'};
+   }
+   close ($fh);
+   
+   if ($sign_mode eq 'smime') {
+       ## Open and parse the file   
+       if (!open(MSG, $tmp_file)) {
+	   &do_log('info', 'Can\'t open %s: %m', $tmp_file);
+	   return undef;
+       }
+    
+       my $parser = new MIME::Parser;
+       $parser->output_to_core(1);
+       my $in_msg;
+       unless ($in_msg = $parser->read(\*MSG)) {
+	   do_log('notice', 'Unable to parse message %s', $file);
+	   return undef;
+       }
+       close MSG;
+       
+       
+       ## Signing the message
+       my $signed_msg ;
+       unless ($signed_msg = &tools::smime_sign($in_msg,$data->{'list'}{'name'}, $data->{'list'}{'dir'})) {
+	   do_log('notice', 'Unable to sign message from %s', $data->{'list'}{'name'});
+	   return undef;
+       }
+       # dump signed message to sendmail
+
+       $signed_msg->print($sendmail);
+       close $sendmail;
+
+   }
+   
+   if (defined $sympa_file) {
+       my $new_file = $sympa_file;
+       $new_file =~ s/T\.//g;
+
+       unless (rename $sympa_file, $new_file) {
+	   &do_log('notice', 'Cannot rename %s to %s : %s', $sympa_file, $new_file, $!);
+	   return undef;
+       }
+   }
+
+   return 1;
+}
+
+
+1;
+
+
+
+
+
+
+
+
+
