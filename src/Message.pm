a9e6e7106a8893a7a0a9567c8a2ce9bad0cf6b9f
olivier.salaun <olivier.salaun@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2007-04-23 11:55:05 +0000

    replacing _ with . in tags/branches names

--- src/Message.pm
+++ src/Message.pm
@@ -1,5 +1,5 @@
-# Message.pm - This module includes Message processing functions
-#<!-- RCS Identication ; $Revision$ ; $Date$ --> 
+# List.pm - This module includes all list processing functions
+# <!-- RCS Identication ; $Revision$ ; $Date$ -->
 
 #
 # Sympa - SYsteme de Multi-Postage Automatique
@@ -20,24 +20,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-=pod 
-
-=head1 NAME 
-
-I<Message.pm> - mail message embedding for internal use in Sympa
-
-=head1 DESCRIPTION 
-
-While processing a message in Sympa, we need to link informations to rhe message, mdify headers and such. This was quite a problem when a message was signed, as modifying anything in the message body would alter its MD5 footprint. And probably make the message to be rejected by clients verifying its identity (which is somehow a good thing as it is the reason why people use MD5 after all). With such messages, the process was complex. We then decided to embed any message treated in a "Message" object, thus making the process easier.
-
-=cut 
-
 package Message;
 
 use strict;
 require Exporter;
 require 'tools.pl';
-require 'tt2.pl';
+require 'parser.pl';
 my @ISA = qw(Exporter);
 my @EXPORT = qw();
 
@@ -48,98 +36,18 @@ use Mail::Internet;
 use Mail::Address;
 use List;
 use MIME::Entity;
-use MIME::EncWords;
+use MIME::Words;
 use MIME::Parser;
 use Conf;
 use Log;
 
-=pod 
-
-=head1 SUBFUNCTIONS 
-
-This is the description of the subfunctions contained by Message.pm
-
-=cut 
-
-
-=pod 
-
-=head2 sub new
-
-Creates a new Message object.
-
-=head3 Arguments 
-
-=over 
-
-=item * I<$pkg>, a package name 
-
-=item * I<$file>, the message file
-
-=item * I<$noxsympato>, a boolean
-
-=back 
-
-=head3 Return 
-
-=over 
-
-=item * I<a Message object>, if created
-
-=item * I<undef>, if something went wrong
-
-=back 
-
-=head3 Calls 
-
-=over 
-
-=item * do_log
-
-=item * Conf::get_robot_conf
-
-=item * List::new
-
-=item * Mail::Address::parse
-
-=item * MIME::EncWords::decode_mimewords
-
-=item * MIME::Entity::as_string
-
-=item * MIME::Head::get
-
-=item * MIME::Parser::output_to_core
-
-=item * MIME::Parser::read
-
-=item * tools::valid_email
-
-=item * tools::smime_decrypt
-
-=item * tools::smime_sign_check
-
-=back 
-
-=cut 
-
 ## Creates a new object
 sub new {
-    my($pkg, $file, $noxsympato) = @_;
-    my $message;
-    &do_log('debug2', 'Message::new(%s,%s)',$file,$noxsympato);
+    my($pkg, $file) = @_;
+    my $message = {'filename' => $file};
+    &do_log('debug2', 'Message::new(%s)', $file);
     
-    if (ref($file) =~ /MIME::Entity/i) {
-	$message->{'msg'} = $file;
-	$message->{'altered'} = '_ALTERED';
-	
-	## Bless Message object
-	bless $message, $pkg;
-	
-	return $message;
-    }
-
     ## Parse message as a MIME::Entity
-    $message->{'filename'} = $file;
     unless (open FILE, $file) {
 	&do_log('err', 'Cannot open message file %s : %s',  $file, $!);
 	return undef;
@@ -147,20 +55,18 @@ sub new {
     
     my $parser = new MIME::Parser;
     $parser->output_to_core(1);
-    
+
     my $msg;
     unless ($msg = $parser->read(\*FILE)) {
 	do_log('err', 'Unable to parse message %s', $file);
 	return undef;
     }
     $message->{'msg'} = $msg;
-    $message->{'msg_as_string'} = $msg->as_string;
-    
+
+    my $hdr = $msg->head;
+
     ## Message size
     $message->{'size'} = -s $file;    
-
-    my $hdr = $message->{'msg'}->head;
-
 
     ## Extract sender address
     unless ($hdr->get('From')) {
@@ -174,57 +80,36 @@ sub new {
     }
     $message->{'sender'} = lc($sender_hdr[0]->address);
 
-    unless (&tools::valid_email($message->{'sender'})) {
-	do_log('err', "Invalid From: field '%s'", $message->{'sender'});
-	return undef;
-    }
-
-    ## Store decoded subject
-    my @decoded_subject = MIME::EncWords::decode_mimewords($hdr->get('Subject'));
-    foreach my $token (@decoded_subject) {
-	$message->{'subject_charset'} ||= $token->[1];
-    }
-    $message->{'decoded_subject'} = MIME::EncWords::decode_mimewords(
-	$hdr->get('Subject'), Charset=>'utf8');
-    chomp $message->{'decoded_subject'};
-
     ## Extract recepient address (X-Sympa-To)
     $message->{'rcpt'} = $hdr->get('X-Sympa-To');
     chomp $message->{'rcpt'};
-    unless (defined $noxsympato) { # message.pm can be used for mesage not in the spool but in archive
-	unless ($message->{'rcpt'}) {
-	    do_log('err', 'no X-Sympa-To found, ignoring message file %s', $file);
-	    return undef;
-	}
+    unless ($message->{'rcpt'}) {
+	do_log('err', 'no X-Sympa-To found, ignoring message file %s', $file);
+	return undef;
+    }
+    
+    ## Strip of the initial X-Sympa-To field
+    # Used by checksum later
+    #$hdr->delete('X-Sympa-To');
+
+    ## Do not check listname if processing a web message
+    unless ($hdr->get('X-Sympa-From')) {
+	## get listname & robot
+	my ($listname, $robot) = split(/\@/,$message->{'rcpt'});
 	
-	## Strip of the initial X-Sympa-To field
-	# Used by checksum later
-	#$hdr->delete('X-Sympa-To');
+	$robot = lc($robot);
+	$listname = lc($listname);
+	$robot ||= $Conf{'host'};
 	
-	## Do not check listname if processing a web message
-	unless ($hdr->get('X-Sympa-From')) {
-	    ## get listname & robot
-	    my ($listname, $robot) = split(/\@/,$message->{'rcpt'});
-	    
-	    $robot = lc($robot);
-	    $listname = lc($listname);
-	    $robot ||= $Conf{'host'};
-	    
-	    my $conf_email = &Conf::get_robot_conf($robot, 'email');
-	    my $conf_host = &Conf::get_robot_conf($robot, 'host');
-	    unless ($listname =~ /^(sympa|$Conf{'listmaster_email'}|$conf_email)(\@$conf_host)?$/i) {
-		my $list_check_regexp = &Conf::get_robot_conf($robot,'list_check_regexp');
+	my $conf_email = &Conf::get_robot_conf($robot, 'email');
+	my $conf_host = &Conf::get_robot_conf($robot, 'host');
+	unless ($listname =~ /^(sympa|listmaster|$conf_email)(\@$conf_host)?$/i) {
+	    my $list_check_regexp = &Conf::get_robot_conf($robot,'list_check_regexp');
 	        if ($listname =~ /^(\S+)-($list_check_regexp)$/) {
 		    $listname = $1;
 		}
-		
-		$message->{'list'} = new List ($listname, $robot);
-		unless ($message->{'rcpt'}) {
-		    do_log('err', 'Could not create List object for list %s in robot %s', $listname, $robot);
-		    return undef;
-		}
-		
-	    }
+	    
+	    $message->{'list'} = new List ($listname, $robot);
 	}
     }
 
@@ -234,26 +119,22 @@ sub new {
 	## Decrypt messages
 	if (($hdr->get('Content-Type') =~ /application\/(x-)?pkcs7-mime/i) &&
 	    ($hdr->get('Content-Type') !~ /signed-data/)){
-	    my ($dec, $dec_as_string) = &tools::smime_decrypt ($message->{'msg'}, $message->{'list'});
-	    
-	    unless (defined $dec) {
-		do_log('debug', "Message %s could not be decrypted", $file);
-		return undef;
-		## We should the sender and/or the listmaster
+	    ($message->{'decrypted_msg'}, $message->{'decrypted_msg_as_string'}) = &tools::smime_decrypt ($msg, $message->{'list'});
+	    if ($message->{'decrypted_msg'}) {
+		$message->{'smime_crypted'} = 1;
+		do_log('debug', "message %s has been decrypted", $file);
 	    }
-
-	    $message->{'smime_crypted'} = 'smime_crypted';
-	    $message->{'orig_msg'} = $message->{'msg'};
-	    $message->{'msg'} = $dec;
-	    $message->{'msg_as_string'} = $dec_as_string;
-	    $hdr = $dec->head;
-	    do_log('debug', "message %s has been decrypted", $file);
-
+	    ## Il faudrait traiter les cas d'erreur (0 différent de undef)
 	}
 	
+	## If message was crypted, now work with decrypted message
+	if ($message->{'smime_crypted'}) {
+	    $msg = $message->{'decrypted_msg'};
+	    $hdr = $msg->head;
+	}
+
 	## Check S/MIME signatures
 	if ($hdr->get('Content-Type') =~ /multipart\/signed|application\/(x-)?pkcs7-mime/i) {
-	    $message->{'protected'} = 1; ## Messages that should not be altered (not footer)
 	    my $signed = &tools::smime_sign_check ($message);
 	    if ($signed->{'body'}) {
 		$message->{'smime_signed'} = 1;
@@ -265,51 +146,11 @@ sub new {
 	
     }
 
-    ## TOPICS
-    my $topics;
-    if ($topics = $hdr->get('X-Sympa-Topic')){
-	$message->{'topic'} = $topics;
-    }
-
     ## Bless Message object
     bless $message, $pkg;
 
     return $message;
 }
-
-=pod 
-
-=head2 sub dump
-
-Dump a Message object to a stream.
-
-=head3 Arguments 
-
-=over 
-
-=item * I<$self>, the Message object to dump
-
-=item * I<$output>, the stream to which dump the object
-
-=back 
-
-=head3 Return 
-
-=over 
-
-=item * I<1>, if everything's alright
-
-=back 
-
-=head3 Calls 
-
-=over 
-
-=item * None
-
-=back 
-
-=cut 
 
 ## Dump the Message object
 sub dump {
@@ -333,111 +174,5 @@ sub dump {
     return 1;
 }
 
-=pod 
-
-=head2 sub add_topic
-
-Add topic and put header X-Sympa-Topic.
-
-=head3 Arguments 
-
-=over 
-
-=item * I<$self>, the Message object to which add a topic
-
-=item * I<$output>, the string containing the topic to add
-
-=back 
-
-=head3 Return 
-
-=over 
-
-=item * I<1>, if everything's alright
-
-=back 
-
-=head3 Calls 
-
-=over 
-
-=item * MIME::Head::add
-
-=back 
-
-=cut 
-
-## Add topic and put header X-Sympa-Topic
-sub add_topic {
-    my ($self,$topic) = @_;
-
-    $self->{'topic'} = $topic;
-    my $hdr = $self->{'msg'}->head;
-    $hdr->add('X-Sympa-Topic', $topic);
-
-    return 1;
-}
-
-
-=pod 
-
-=head2 sub add_topic
-
-Add topic and put header X-Sympa-Topic.
-
-=head3 Arguments 
-
-=over 
-
-=item * I<$self>, the Message object whose topic is retrieved
-
-=back 
-
-=head3 Return 
-
-=over 
-
-=item * I<the topic>, if it exists
-
-=item * I<empty string>, otherwise
-
-=back 
-
-=head3 Calls 
-
-=over 
-
-=item * MIME::Head::add
-
-=back 
-
-=cut 
-
-## Get topic
-sub get_topic {
-    my ($self) = @_;
-
-    if (defined $self->{'topic'}) {
-	return $self->{'topic'};
-
-    } else {
-	return '';
-    }
-}
-
-
 ## Packages must return true.
 1;
-=pod 
-
-=head1 AUTHORS 
-
-=over 
-
-=item * Serge Aumont <sa AT cru.fr> 
-
-=item * Olivier Salaün <os AT cru.fr> 
-
-=back 
-
-=cut 
