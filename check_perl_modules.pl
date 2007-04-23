d00bc70eb294805012816c79d6fd7f9eca1eed3b
olivier.salaun <olivier.salaun@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2007-04-23 11:54:40 +0000

    replacing _ with . in tags/branches names

--- check_perl_modules.pl
+++ check_perl_modules.pl
@@ -1,115 +1,61 @@
-# check_perl_modules.pl - This script checks installed and required Perl modules
-# It also does the required installations
-# RCS Identication ; $Revision$ ; $Date$ 
+# check for perl and modules for SYMPA
 #
-# Sympa - SYsteme de Multi-Postage Automatique
-# Copyright (c) 1997, 1998, 1999, 2000, 2001 Comite Reseau des Universites
-# Copyright (c) 1997,1998, 1999 Institut Pasteur & Christophe Wolfhugel
+# Laurent Ghys (Laurent.Ghys@ircam.fr)
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# Changes
+# 04/03/2001 [OS] Add DBD drivers
+#                 Perl 5.005 is required because of CipherSaber
+# 03/02/2001 [SA] Add Cipher:Saber as a optional module
+# 03/08/2000 [O Salaun] arg passed to CPAN::Shell->install() is the module name
+# 02/08/2000 [O Salaun] require CGI 2.52 (Vars())
+# 26/07/2000 [O Salaun] corrected MIME::Tools to MIME::tools
+# 13/07/2000 [L Ghys]   sort %todo to avoid error message with CGI::Fast
+# 10/07/2000 [O Salaun] MIME::tools now require File::Spec
+# 19/06/2000 [O Salaun] require Net::LDAP 0.19
+# 05/06/2000 [O Salaun] Installing required modules
+# 31/05/2000 [O Salaun] Added FCGI & CGI::Fast
+# 12/05/99 [O Salaun] Net-LDAP changed to Netperl-ldap 
+# 20/05/99 [L Ghys]   added stuff for CPAN modules
 
 use CPAN;
 
 ## assume version = 1.0 if not specified.
 ## 
 %versions = ('perl' => '5.005',
-             'Net::LDAP' =>, '0.27', 
+             'Net::LDAP' =>, '0.10', 
 	     'perl-ldap' => '0.10',
-	     'Mail::Internet' => '1.51', 
+	     'Mail::Internet' => '1.32', 
 	     'DBI' => '1.06',
 	     'DBD::Pg' => '0.90',
 	     'DBD::Sybase' => '0.90',
-	     'DBD::mysql' => '2.0407',
-	     'FCGI' => '0.67',
-	     'MIME::Tools' => '5.419',
+	     'FCGI' => '0.48',
+	     'MIME::Tools' => '5.209',
 	     'File::Spec' => '0.8',
              'Crypt::CipherSaber' => '0.50',
 	     'CGI' => '2.52',
-	     'Digest::MD5' => '2.00',
-	     'DB_File' => '1.75',
-	     'IO::Socket::SSL' => '0.90',
-	     'Net::SSLeay' => '1.16',
-	     'Archive::Zip' => '1.05',
-	     'Bundle::LWP' => '1.09',
-	     'SOAP::Lite' => '0.60',
-	     'MHonArc::UTF8' => '2.6.0',
-	     'MIME::Base64' => '3.03',
-	     'Crypt::OpenSSL::X509' => '0.3.1',
-	     'MIME::Charset' => '0.04.1',
-	     'MIME::EncWords' => '0.040',
-	     );
+	     'Digest::MD5' => '2.00');
 
 ### key:left "module" used by SYMPA, 
 ### right CPAN module.		     
 %req_CPAN = ('DB_File' => 'DB_FILE',
+	     'Locale::Msgcat' => 'Msgcat',
 	     'Digest::MD5' => 'Digest-MD5',
 	     'Mail::Internet' =>, 'MailTools',
 	     'IO::Scalar' => 'IO-stringy',
 	     'MIME::Tools' => 'MIME-tools',
 	     'MIME::Base64' => 'MIME-Base64',
 	     'CGI' => 'CGI',
-	     'File::Spec' => 'File-Spec',
-	     'Regexp::Common' => 'Regexp-Common',
-	     'Locale::TextDomain' => 'libintl-perl',
-	     'Template' => 'Template-Toolkit',
-	     'Archive::Zip' => 'Archive-Zip',
-	     'LWP' => 'libwww-perl',
-             'XML::LibXML' => 'XML-LibXML',
-	     'MHonArc::UTF8' => 'MHonArc',
-	     'FCGI' => 'FCGI',
-	     'DBI' => 'DBI',
-	     'DBD::mysql' => 'Msql-Mysql-modules',
-	     'Crypt::CipherSaber' => 'CipherSaber',
-	     'Encode' => 'Encode',
-	     'MIME::Charset' => 'MIME-Charset',
-	     'MIME::EncWords' => 'MIME-EncWords',
-	     );
+	     'File::Spec' => 'File-Spec');
 
-%opt_CPAN = ('DBD::Pg' => 'DBD-Pg',
+%opt_CPAN = ('DBI' => 'DBI',
+	     'DBD::mysql' => 'Msql-Mysql-modules',
+	     'DBD::Pg' => ' DBD-Pg',
 	     'DBD::Oracle' => 'DBD-Oracle',
 	     'DBD::Sybase' => 'DBD-Sybase',
-	     'DBD::SQLite' => 'DBD-SQLite',
 	     'Net::LDAP' =>   'perl-ldap',
 	     'CGI::Fast' => 'CGI',
- 	     'Net::SMTP' => 'libnet',
-	     'IO::Socket::SSL' => 'IO-Socket-SSL',
-	     'Net::SSLeay' => 'NET-SSLeay',
-	     'Bundle::LWP' => 'LWP',
-	     'SOAP::Lite' => 'SOAP-Lite',
-	     'Crypt::OpenSSL::X509' => 'Crypt-OpenSSL-X509',
-	     'File::NFSLock' => 'File-NFSLock');
-
-%opt_features = ('DBI' => 'a generic Database Driver, required by Sympa to access Subscriber information and User preferences. An additional Database Driver is required for each database type you wish to connect to.',
-		 'DBD::mysql' => 'Mysql database driver, required if you connect to a Mysql database.\nYou first need to install the Mysql server and have it started before installing the Perl DBD module.',
-		 'DBD::Pg' => 'PostgreSQL database driver, required if you connect to a PostgreSQL database.',
-		 'DBD::Oracle' => 'Oracle database driver, required if you connect to a Oracle database.',
-		 'DBD::Sybase' => 'Sybase database driver, required if you connect to a Sybase database.',
-		 'DBD::SQLite' => 'Sybase database driver, required if you connect to a SQLite database.',
-		 'Net::LDAP' =>   'required to query LDAP directories. Sympa can do LDAP-based authentication ; it can also build mailing lists with LDAP-extracted members.',
-		 'CGI::Fast' => 'WWSympa, Sympa\'s web interface can run as a FastCGI (ie: a persistent CGI). If you install this module, you will also need to install the associated mod_fastcgi for Apache.',
-		 'Crypt::CipherSaber' => 'this module provides reversible encryption of user passwords in the database.',
-		 'Archive::Zip ' => 'this module provides zip/unzip for archive and shared document download/upload',
-		 'FCGI' => 'WSympa, Sympa\'s web interface can run as a FastCGI (ie: a persistent CGI). If you install this module, you will also need to install the associated mod_fastcgi for Apache.',
-		 'Net::SMTP' => 'this is required if you set \'list_check_smtp\' sympa.conf parameter, used to check existing aliases before mailing list creation.',
-		 'IO::Socket::SSL' => 'required by CAS (single sign-on) and the \'include_remote_sympa_list\' feature that includes members of a list on a remote server, using X509 authentication',
-		 'Net::SSLeay' => 'required by the \'include_remote_sympa_list\' feature that includes members of a list on a remote server, using X509 authentication',
-		 'Bundle::LWP' => 'required by the \'include_remote_sympa_list\' feature that includes members of a list on a remote server, using X509 authentication',
-		 'SOAP::Lite' => 'required if you want to run the Sympa SOAP server that provides ML services via a "web service"',
-		 'Crypt::OpenSSL::X509' => 'required for HTTP x509 authentication (when using Email in SubjAltName)',
-		 'File::NFSLock' => 'required to perform NFS lock ; see also lock_method sympa.conf parameter'
-		 );
+             'Crypt::CipherSaber' => 'CipherSaber',
+	     'FCGI' => 'FCGI');
 
 ### main:
 print "******* Check perl for SYMPA ********\n";
@@ -144,13 +90,9 @@ sub check_modules {
 
     foreach $mod (sort keys %todo) {
 	printf ("%-20s %-15s", $mod, $todo{$mod});
-	
 	$status = &test_module($mod);
 	if ($status == 1) {
 	    $vs = "$mod" . "::VERSION";
-
-	    $vs = 'mhonarc::VERSION' if $mod =~ /^mhonarc/i;
-
 	    $v = $$vs;
 	    $rv = $versions{$mod} || "1.0" ;
 	    ### OK: check version
@@ -164,7 +106,7 @@ sub check_modules {
 	    }
 	} elsif ($status eq "nofile") {
 	    ### not installed
-	    print "was not found on this system.\n";
+	    print "seems to be not available on this system.\n";
 
 	    &install_module($mod, $default);
 
@@ -172,7 +114,7 @@ sub check_modules {
 	    ### doesn't return 1;
 	    print "$mod doesn't return 1 (check it).\n";
 	} else {
-	    print "$status\n";
+	    print "$status";
 	}
     }
 }
@@ -183,15 +125,6 @@ sub check_modules {
 sub install_module {
     my ($module, $default) = @_;
 
-    unless ($ENV{'FTP_PASSIVE'} eq 1) {
-	$ENV{'FTP_PASSIVE'} = 1;
-	print "Setting FTP Passive mode\n";
-    }
-
-    ## This is required on RedHat 9 for DBD::mysql installation
-    my $lang = $ENV{'LANG'};
-    $ENV{'LANG'} = 'C' if ($ENV{'LANG'} =~ /UTF\-8/);
-
     unless ($> == 0) {
 	print "\#\# You need root privileges to install $module module. \#\#\n";
 	print "\#\# Press the Enter key to continue checking modules. \#\#\n";
@@ -199,19 +132,12 @@ sub install_module {
 	return undef;
     }
 
-    printf "Description: %s\n", $opt_features{$module};
     print "Install module $module ? [$default]";
     my $answer = <STDIN>; chomp $answer;
     $answer ||= $default;
     next unless ($answer =~ /^y$/i);
-    $CPAN::Config->{'inactivity_timeout'} = 4;
-    CPAN::Shell->make($module);
-    CPAN::Shell->test($module);
-    CPAN::Shell->install($module); ## Could use CPAN::Shell->force('install') if make test failed
-
-    ## Restore lang
-    $ENV{'LANG'} = $lang if (defined $lang);
-
+  CPAN::Shell->conf('inactivity_timeout', 4);
+    CPAN::Shell->install($module);
 }
 
 ###--------------------------
@@ -224,10 +150,6 @@ sub test_module {
 
     $filename =~ s/::/\//g;
     $filename .= ".pm";
-    
-    ## Exception for mhonarc
-    $filename = 'mhamain.pl' if $filename =~ /^mhonarc/i;
-
     return 1 if $INC{$filename};
     
   ITER: {
---
+++ wwsympa/wws_templates/fr/d_read.fr.tpl
@@ -0,0 +1,297 @@
+<!-- RCS Identication ; $Revision$ ; $Date$ -->
+
+[IF file]
+  [INCLUDE file]
+[ELSE]
+
+  [IF path]  
+    <h2><B><IMG SRC="/icons/folder.open.gif"> [path] </B> </h2> 
+    <A HREF="[path_cgi]/d_editfile/[list]/[escaped_path]">éditer</A> |
+    <A HREF="[path_cgi]/d_delete/[list]/[escaped_path]" onClick="request_confirm_link('[path_cgi]/d_delete/[list]/[escaped_path]', 'Voulez-vous vraiment supprimer [path] ?'); return false;">supprimer</A> |
+<A HREF="[path_cgi]/d_control/[list]/[escaped_path]">accès</A><BR>
+    Propriétaire : [doc_owner] <BR>
+    Mise à jour : [doc_date] <BR>
+    [IF doc_title]
+    Description : [doc_title] <BR><BR>
+    [ENDIF]
+    <font size=+1> <A HREF="[path_cgi]/d_read/[list]/[escaped_father]"> <IMG ALIGN="bottom"  src="[father_icon]" BORDER="0"> Dossier parent</A></font>
+    <BR>  
+  [ELSE]
+    <h2> <B> Liste des documents</B> </h2> 
+  [ENDIF]
+   
+  <TABLE width=100% CELLPADDING="0">
+  <TR BGCOLOR="[dark_color]">
+   
+  <th><TABLE width=100% CELLPADDING="0"><TR><TD ALIGN="left"><font color="[bg_color]">Document</font></TD>
+  [IF  order_by<>order_by_doc]  
+    <TD ALIGN="right">
+    <form method="post" ACTION="[path_cgi]">  
+    <INPUT ALIGN="top"  type="image" src="[sort_icon]" WIDTH=15 HEIGHT=15 name="Sort by name">
+    <INPUT TYPE="hidden" NAME="list" VALUE="[list]">
+    <INPUT TYPE="hidden" NAME="path" VALUE="[path]">
+    <INPUT TYPE="hidden" NAME="action" VALUE="d_read">
+    <INPUT TYPE="hidden" NAME="order" VALUE="order_by_doc">
+    </form>
+    </TD>
+  [ENDIF]	
+  </TR></TABLE>
+  </th>
+  
+  <th><TABLE width=100% CELLPADDING="0"><TR><TD ALIGN="left"><font color="[bg_color]">Auteur</font></TD>
+  [IF  order_by<>order_by_author]  
+    <TD ALIGN="right">
+    <form method="post" ACTION="[path_cgi]">  
+    <INPUT ALIGN="top"  type="image" src="[sort_icon]" WIDTH=15 HEIGHT=15 name="Sort by author">
+    <INPUT TYPE="hidden" NAME="list" VALUE="[list]">
+    <INPUT TYPE="hidden" NAME="path" VALUE="[path]">
+    <INPUT TYPE="hidden" NAME="action" VALUE="d_read">
+    <INPUT TYPE="hidden" NAME="order" VALUE="order_by_author">
+    </form>	
+    </TD>
+  [ENDIF]
+  </TR></TABLE>
+  </th> 
+
+  <th><TABLE width=100% CELLPADDING="0"><TR><TD ALIGN="left"><font color="[bg_color]">Taille (Ko)</font></TD>
+  [IF order_by<>order_by_size] 
+    <TD ALIGN="right">
+    <form method="post" ACTION="[path_cgi]">
+    <INPUT ALIGN="top"  type="image" src="[sort_icon]" WIDTH=15 HEIGHT=15 name="Sort by size">
+    <INPUT TYPE="hidden" NAME="list" VALUE="[list]">
+    <INPUT TYPE="hidden" NAME="path" VALUE="[path]">
+    <INPUT TYPE="hidden" NAME="action" VALUE="d_read">
+    <INPUT TYPE="hidden" NAME="order" VALUE="order_by_size">
+    </form>
+    </TD>
+  [ENDIF]
+  </TR></TABLE>   
+  </th> 
+
+  <th><TABLE width=100% CELLPADDING="0"><TR><TD ALIGN="left"><font color="[bg_color]">Mise à jour</font></TD>
+  [IF order_by<>order_by_date]
+    <TD ALIGN="right">
+    <form method="post" ACTION="[path_cgi]">
+    <INPUT ALIGN="top"  type="image" src="[sort_icon]" WIDTH=15 HEIGHT=15 name="Sort by date">
+    <INPUT TYPE="hidden" NAME="list" VALUE="[list]">
+    <INPUT TYPE="hidden" NAME="path" VALUE="[path]">
+    <INPUT TYPE="hidden" NAME="action" VALUE="d_read">
+    <INPUT TYPE="hidden" NAME="order" VALUE="order_by_date">
+    </form>
+    </TD>
+  [ENDIF]
+  </TR></TABLE>  
+  </th> 
+
+  <TD ALIGN="center"><font color="[bg_color]">Editer</font></TD> 
+  <TD ALIGN="center"><font color="[bg_color]">Supprimer</font></TD>
+  <TD ALIGN="center"><font color="[bg_color]">Accès</font></TD></TR>
+      
+  [IF empty]
+    <TR BGCOLOR="[light_color]">
+    <TD COLSPAN=8 ALIGN="center"> Dossier vide </TD>
+    </TR>
+  [ELSE]   
+    [IF sort_subdirs]
+      [FOREACH s IN sort_subdirs] 
+        <TR BGCOLOR="[light_color]">        
+	<TD NOWRAP> <A HREF="[path_cgi]/d_read/[list]/[escaped_path][s->escaped_doc]/"> 
+	<IMG ALIGN=bottom BORDER=0 SRC="[s->icon]" ALT="[s->title]"> [s->doc]</A></TD>
+	<TD>
+	[IF s->author_known] 
+	  <A HREF="mailto:[s->author]">[s->author]</A>  
+        [ELSE]
+	   inconnu
+	[ENDIF]
+	</TD>	    
+	<TD>&nbsp;</TD>
+	<TD> [s->date] </TD>
+		
+	[IF s->edit]
+	<TD><center>
+	<font size=-1>
+	<A HREF="[path_cgi]/d_editfile/[list]/[escaped_path][s->escaped_doc]">éditer</A>
+	</font>
+	</center></TD>
+	
+	  <TD><center>
+	  <FONT size=-1>
+	  <A HREF="[path_cgi]/d_delete/[list]/[escaped_path][s->escaped_doc]" onClick="request_confirm_link('[path_cgi]/d_delete/[list]/[escaped_path][s->escaped_doc]', 'Voulez-vous vraiment supprimer [path][s->doc] ?'); return false;">supprimer</A>
+	  </FONT>
+	  </center></TD>
+	[ELSE]
+	  <TD>&nbsp; </TD>
+	  <TD>&nbsp; </TD>
+	[ENDIF]
+	
+	[IF s->control]
+	  <TD>
+	  <center>
+	  <FONT size=-1>
+	  <A HREF="[path_cgi]/d_control/[list]/[escaped_path][s->escaped_doc]">accès</A>
+	  </font>
+	  </center>
+	  </TD>	 
+	[ELSE]
+	  <TD>&nbsp; </TD>
+	[ENDIF]
+      </TR>
+      [END] 
+    [ENDIF]
+
+    [IF sort_files]
+      [FOREACH f IN sort_files]
+        <TR BGCOLOR="[light_color]"> 
+        <TD NOWRAP>
+        [IF f->html]
+	  <A HREF="[path_cgi]/d_read/[list]/[escaped_path][f->escaped_doc]" TARGET="html_window">
+	  <IMG ALIGN=bottom BORDER=0 SRC="[f->icon]" ALT="[f->title]"> [f->doc] </A>
+	[ELSIF f->url]
+	  <A HREF="[f->url]" TARGET="html_window">
+	  <IMG ALIGN=bottom BORDER=0 SRC="[f->icon]" ALT="[f->title]"> [f->anchor] </A>
+	[ELSE]
+	  <A HREF="[path_cgi]/d_read/[list]/[escaped_path][f->escaped_doc]">
+	  <IMG ALIGN=bottom BORDER=0 SRC="[f->icon]" ALT="[f->title]"> [f->doc] </A>
+        [ENDIF] 
+	</TD>  
+	 
+	<TD> 
+	[IF f->author_known]
+	  <A HREF="mailto:[f->author]">[f->author]</A>  
+	[ELSE]
+          inconnu  
+        [ENDIF]
+	</TD>
+	 
+	<TD NOWRAP>&nbsp;
+	[IF !f->url]
+	[f->size] 
+	[ENDIF]
+	</TD>
+	<TD> [f->date] </TD>
+	 
+	[IF f->edit]
+	<TD>
+	<center>
+	<font size=-1>
+	<A HREF="[path_cgi]/d_editfile/[list]/[escaped_path][f->escaped_doc]">éditer</A>
+	</font>
+	</center>
+
+	</TD>
+	<TD>
+	<center>
+	<FONT size=-1>
+	<A HREF="[path_cgi]/d_delete/[list]/[escaped_path][f->escaped_doc]" onClick="request_confirm_link('[path_cgi]/d_delete/[list]/[escaped_path][f->escaped_doc]', 'Voulez-vous vraiment supprimer [path][f->doc] ([f->size] Ko) ?'); return false;">supprimer</A>
+	</FONT>
+	</center>
+	</TD>
+	[ELSE]
+	  <TD>&nbsp; </TD> <TD>&nbsp; </TD>
+	[ENDIF]
+		 
+	[IF f->control]
+	  <TD> <center>
+	  <font size=-1>
+	  <A HREF="[path_cgi]/d_control/[list]/[escaped_path][f->escaped_doc]">accès</A>
+	  </font>
+	  </center></TD>
+	[ELSE]
+	<TD>&nbsp; </TD>
+	[ENDIF]
+	</TD>
+	</TR>
+      [END] 
+    [ENDIF]
+  [ENDIF]
+  </TABLE>	        
+ 
+  <HR> 
+<TABLE CELLSPACING=20>
+   
+  [IF may_edit]
+    <TR>
+    <FORM METHOD="POST" ACTION="[path_cgi]">
+    <TD ALIGN="right" VALIGN="bottom">
+    [IF path]
+      <B> Créer un sous dossier dans [path]</B> <BR>
+    [ELSE]
+      <B> Créer un dossier</B> <BR>
+    [ENDIF]
+    <input MAXLENGTH=30 type="text" name="name_doc">
+    </TD>
+
+    <TD ALIGN="left" VALIGN="bottom">
+    <input type="submit" value="Créer" name="action_d_create_dir">
+    <INPUT TYPE="hidden" NAME="previous_action" VALUE="d_read">
+    <INPUT TYPE="hidden" NAME="list" VALUE="[list]">
+    <INPUT TYPE="hidden" NAME="path" VALUE="[path]">
+    <INPUT TYPE="hidden" NAME="type" VALUE="directory">
+    <INPUT TYPE="hidden" NAME="action" VALUE="d_create_dir">
+    </TD>
+    </FORM>
+    </TR>
+
+    <TR>
+    <FORM METHOD="POST" ACTION="[path_cgi]">
+    <TD ALIGN="right" VALIGN="bottom">
+      <B> Créer un fichier</B> <BR>
+    <input MAXLENGTH=30 type="text" name="name_doc">
+    </TD>
+
+    <TD ALIGN="left" VALIGN="bottom">
+    <input type="submit" value="Créer" name="action_d_create_dir">
+    <INPUT TYPE="hidden" NAME="previous_action" VALUE="d_read">
+    <INPUT TYPE="hidden" NAME="list" VALUE="[list]">
+    <INPUT TYPE="hidden" NAME="path" VALUE="[path]">
+    <INPUT TYPE="hidden" NAME="type" VALUE="file">
+    <INPUT TYPE="hidden" NAME="action" VALUE="d_create_dir">
+    </TD>
+    </FORM>
+    </TR>
+
+    <TR>
+    <FORM METHOD="POST" ACTION="[path_cgi]">
+    <TD ALIGN="right" VALIGN="center">
+    <B>Ajouter un signet</B><BR>
+    URL <input MAXLENGTH=100 SIZE="25" type="text" name="url"><BR>
+    intitulé <input MAXLENGTH=100 SIZE="20" type="text" name="name_doc">
+    </TD>
+
+    <TD ALIGN="left" VALIGN="bottom">
+    <input type="submit" value="Ajouter" name="action_d_savefile">
+    <INPUT TYPE="hidden" NAME="previous_action" VALUE="d_read">
+    <INPUT TYPE="hidden" NAME="list" VALUE="[list]">
+    <INPUT TYPE="hidden" NAME="path" VALUE="[path]">
+    <INPUT TYPE="hidden" NAME="action" VALUE="d_savefile">
+    </TD>
+    </FORM>
+    </TR>
+
+   <TR>
+   <FORM METHOD="POST" ACTION="[path_cgi]" ENCTYPE="multipart/form-data">
+   <TD ALIGN="right" VALIGN="bottom">
+   [IF path]
+     <B> Télécharger un fichier dans le dossier [path]</B><BR>
+   [ELSE]
+     <B> Télécharger un fichier </B><BR>
+   [ENDIF]
+   <input type="file" name="uploaded_file">
+   </TD>
+
+   <TD ALIGN="left" VALIGN="bottom">
+   <input type="submit" value="Publier" name="action_d_upload">
+   <INPUT TYPE="hidden" NAME="list" VALUE="[list]">
+   <INPUT TYPE="hidden" NAME="path" VALUE="[path]">
+   <TD>
+   </FORM>
+   </TR>
+   [ENDIF]
+</TABLE>
+</FORM>
+[ENDIF]
+   
+
+
+
+
