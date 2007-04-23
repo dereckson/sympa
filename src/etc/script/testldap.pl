05d640c8fb3cca54724a32978d368783b61ea0fc
olivier.salaun <olivier.salaun@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2007-04-23 11:53:51 +0000

    replacing _ with . in tags/branches names

--- src/etc/script/testldap.pl
+++ src/etc/script/testldap.pl
@@ -1,63 +1,20 @@
-#!--PERL--
+#!/usr/bin/perl
 
-# testldap.pl - This script aims at testing LDAP queries
-# RCS Identication ; $Revision$ ; $Date$ 
-#
-# Sympa - SYsteme de Multi-Postage Automatique
-# Copyright (c) 1997, 1998, 1999, 2000, 2001 Comite Reseau des Universites
-# Copyright (c) 1997,1998, 1999 Institut Pasteur & Christophe Wolfhugel
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-
-use Getopt::Long;
-
-my %opt;
-unless (&GetOptions(\%opt, 'host=s', 'suffix=s', 'filter=s','attrs=s', 'ssl=s', 'scope=s')) {
-    die "Unknown options.";
+unless ($#ARGV == 3) {
+   die "Usage $ARGV[-1] <host> <suffix> <filter> <attrs>";
 }
 
-unless (defined $opt{'host'} &&
-	defined $opt{'suffix'} &&
-	defined $opt{'filter'}) {
-    die "Usage $ARGV[-1] --host=<host> --ssl=on|off --suffix=<suffix> --scope=base|one|sub --filter=<filter> --attrs=<attrs>";
-}
+($directory, $suffix, $filter, $attrs) = @ARGV;
 
-printf "host : $opt{'host'}\nsuffix : $opt{'suffix'}\nfilter: $opt{'filter'}\n";
-
-my %arg;
-$arg{'scope'} = $opt{'scope'};
-
-
-if ($opt{'ssl'} eq 'on') {
-    eval "require Net::LDAPS";
-
-    $arg{'sslversion'} = 'sslv3';
-    $arg{'sslciphers'} = 'ALL';
-
-    $ldap=Net::LDAPS->new($opt{'host'},%arg) or print "connect impossible\n";
-}else {
-    use Net::LDAP;
-    $ldap=Net::LDAP->new($opt{'host'},%arg) or print "connect impossible\n";
-}
-
+printf "host : $directory\nsuffix : $suffix\nfilter: $filter\n";
+use Net::LDAP;
+$ldap=Net::LDAP->new($directory) or print "connect impossible\n";
 $ldap->bind or print "bind impossible \n";
 
-#$mesg = $ldap->search ( base => "$opt{'suffix'}", filter => "(cn=$nom)" )
-$mesg = $ldap->search ( base => $opt{'suffix'}, 
-			filter => $opt{'filter'}, 
-			attrs => [$opt{'attrs'}] )
+#$mesg = $ldap->search ( base => "$suffix", filter => "(cn=$nom)" )
+$mesg = $ldap->search ( base => "$suffix", 
+			filter => "$filter", 
+			attrs => [$attrs] )
 or  print "Search  impossible \n";
 
 # $mesg->code or  print "code chjie\n";
