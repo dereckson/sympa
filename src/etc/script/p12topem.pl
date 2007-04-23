a5ce26389909019d584200d256747242d73ebaf7
olivier.salaun <olivier.salaun@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2007-04-23 11:54:12 +0000

    replacing _ with . in tags/branches names

--- src/etc/script/p12topem.pl
+++ src/etc/script/p12topem.pl
@@ -23,87 +23,69 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
+($p12input,$listname) = @ARGV;
 
-use strict;
 
-use lib '--LIBDIR--';
-use Getopt::Long;
-
-my $sympa_conf_file = '--CONFIG--';
+use lib '--BINDIR--';
+$sympa_conf_file = '--CONFIG--';
 use Conf;
-use List;
 
 ## Load sympa config
 unless (&Conf::load($sympa_conf_file)) {
     die 'config_error';
 }
+my $openssl = $Conf::Conf{'openssl'};
+my $home_sympa = $Conf::Conf{'home'};
+my $outpass = $Conf::Conf{'key_passwd'};
 
-## Probe Db if defined
-if ($Conf{'db_name'} and $Conf{'db_type'}) {
-    unless ($List::use_db = &Upgrade::probe_db()) {
-	&die('Database %s defined in sympa.conf has not the right structure or is unreachable. If you don\'t use any database, comment db_xxx parameters in sympa.conf', $Conf{'db_name'});
-    }
-}
+if (($p12input =~ /help$/) || ($#ARGV != 1)) {
+printf "
 
-## Apply defaults to %List::pinfo
-&List::_apply_defaults();
+Usage $ARGV[-1] <pkcs#12cert> <listname>
 
-my $openssl = $Conf{'openssl'};
-my $home_sympa = $Conf{'home'};
-my $outpass = $Conf{'key_passwd'};
-my $etc_dir = $Conf{'etc'};
+This script is intended to convert a PKCS#12 certificates in PEM format
+using Openssl. This is usefull because most PKI providerd deliver certificates
+using a web interface so the certificat is stored in your browser.
 
-## Check option
-my %options;
-&GetOptions(\%main::options, 'pkcs12=s','listname=s', 'robot=s', 'help|h');
+When exporting a certificate from Netscape the result is stored using
+PKCS#12 format.
 
-$main::options{'foreground'} = 1;
-my $listname = $main::options{'listname'};
-my $robot = $main::options{'robot'};
-my $p12input = $main::options{'pkcs12'};
+Sympa requires a pair of PEM certificat and private key. You must then convert
+your pkcs#12 into PEM :
+ - $home_sympa/<listname>/cert.pem
+ - $home_sympa/<listname>/private_key
 
+This can be done using  $ARGV[-1] <pkcs#12cert> <listname>
 
-my ($cert,$privatekey,$inpass,$key);
+You are then prompted for inpassword (the password used to encrypt the
+pkc#12 file).\n";
+unless ($outpass) {
+printf "Because Sympa's password \"key_passwd\" is not configured in sympa.conf you will
+also be prompted for the password used by sympa to access to the list private key)\n";
+} 
 
-if (($main::options{'help'} ne '') || 
-    !(-r $main::options{'pkcs12'}) || 
-    (($main::options{'listname'} ne '') && ($main::options{'robot'} ne ''))
-   ){
-    &print_usage(); 
 }else{
 
-    if ($listname) {
-	my $self = new List($listname, $robot);
-	$cert = $self->{'dir'}.'/cert.pem';
-	$privatekey = $self->{'dir'}.'/private_key';
-	unless (-d $self->{'dir'}) {
-	    printf "unknown list $listname (directory $self->{'dir'} not found)\n";
-	    die;
-	}
-    }elsif($robot) {
-	if (-d $Conf{'etc'}.'/'.$robot) {
-	    $cert = $Conf{'etc'}.'/'.$robot.'/cert.pem';
-	    $privatekey = $Conf{'etc'}.'/'.$robot.'/private_key';
-	}else{
-	    $cert = $Conf{'etc'}.'/cert.pem';
-	    $privatekey = $Conf{'etc'}.'/private_key';	    
-	}
-    }
+    $cert = "$home_sympa/$listname/cert.pem";
+    $privatekey = "$home_sympa/$listname/private_key";
 
+    unless (-d "$home_sympa/$listname") {
+	printf "unknown list $listname (directory $home_sympa/$listname not found)\n";
+        die;
+    }
     if (-r "$cert") {
-	printf "$cert certificat already exist\n";
-	die;
+	printf "$listname list X509 certificat allready exist ($cert)\n";
+        die;
     }
     if (-r "$privatekey") {
-	printf "$privatekey already exist\n";
-	die;
+	printf "$listname list privatekey allready exist ($privatekey)\n";
+        die;
     }
-    
     unless ($openssl) {
 	printf "You must first configure Sympa to use openssl. Check the parameter openssl in sympa.conf\n";
-	die;
+        die;
     }
-    
+
     system 'stty', '-echo';
     printf "password to access to $p12input :";
     chop($inpass = <STDIN>);
@@ -112,7 +94,7 @@ if (($main::options{'help'} ne '') ||
     open  PASS, "| $openssl pkcs12 -in $p12input -out $cert -nokeys -clcerts -passin stdin";
     print PASS "$inpass\n";
     close PASS ;
-    
+
     unless ($outpass) {
 	system 'stty', '-echo';
 	printf "sympa password to protect list private_key $key:";
@@ -123,40 +105,11 @@ if (($main::options{'help'} ne '') ||
     open  PASS, "| $openssl pkcs12 -in $p12input -out $privatekey -nocerts -passin stdin -des3 -passout stdin";
     print PASS "$inpass\n$outpass\n";
     close PASS ;
-    
-    printf "$privatekey and  $cert created.\n";
-    exit;
+
+    printf "
+$privatekey and  $cert created. Now welcome message for list $listname will be signed\n
+using S/MIME. Encrypted messages will be distributed in a crypted form to each subscriber\n
+using their X509 certificat.\n";
 }
 
 
-
-sub print_usage {
-printf "
-
-Usage p12topem.pl --pkcs12 <pkcs#12_cert_file> --listname <listname> or
-      p12topem.pl --pkcs12 <pkcs#12_cert_file> --robot <robot>
-
-This script is intended to convert a PKCS#12 certificates in PEM format
-using Openssl. This is usefull because most PKI providerd deliver certificates
-using a web interface so the certificat is stored in your browser.
-
-When exporting a certificate from a browser (Netscape, IE, Mozilla etc)
-the result is stored using PKCS#12 format.Sympa requires a pair of PEM
-certificat and private key. You must then convert your pkcs#12 into PEM. 
-
-For a list certificat, the file will be installed in
-$home_sympa/<listname>/cert.pem and $home_sympa/<listname>/private_key
-
-For Sympa itself a certificate will be installed in 
-$Conf{'etc'}/<robot>/cert.pem and  $Conf{'etc'}/<robot>/private_key or
-$Conf{'etc'}/cert.pem and Conf{'etc'}/private_key
-
-
-You are then prompted for inpassword (the password used to encrypt the
-pkc#12 file).\n";
-unless ($outpass) {
-printf "Because you did not configure Sympa's password \"key_passwd\"  in
-sympa.conf you will also be prompted for the password used by sympa to access
-to the list private key)\n";
-} 
-}
